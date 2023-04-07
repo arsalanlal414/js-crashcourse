2e4722184ca2d820d8836d31331673df342b4428
Muhammad Arsalan <54612901+arsalanlal414@users.noreply.github.com>
2023-04-07 15:17:07 +0500

    Add files via upload

---
+++ assignment01/app.js
@@ -0,0 +1,100 @@
+// task 01
+
+// let input1 = +prompt("enter 1st number")
+// let input2 = +prompt("enter 2nd number")
+
+// if(input1 > input2){
+//     console.log(input1);
+// }else if(input2 > input1){
+//     console.log(input2);
+// }else{
+//     console.log("both are equal", input1);
+// }
+
+// task 02
+
+// let input = +prompt("enter the number positive or negative.")
+// if(input>=0){
+//     console.log("the sign is +");
+// }else{
+//     console.log("the sign is -");
+// }
+
+
+// task 03
+
+// let input1 = +prompt("enter 1st number")
+// let input2 = +prompt("enter 2nd number")
+// let input3 = +prompt("enter 3rd number")
+// let input4 = +prompt("enter 4th number")
+// let input5 = +prompt("enter 5th number")
+
+// if(input1 > input2 && input1 > input3 && input1 > input4 && input1 > input5 ){
+//     console.log(input1);
+// }else if(input2 > input1 && input2 > input3 && input2 > input4 && input2 > input5){
+//     console.log(input2);
+// }else if(input3 > input1 && input3 > input2 && input3 > input4 && input3 > input5){
+//     console.log(input3);
+// }else if(input4 > input1 && input4 > input2 && input4 > input3 && input4 > input5){
+//     console.log(input4);
+// }else{
+//     console.log(input5);
+// }
+    
+// task 04
+
+// for(let i=0; i<=15; i++){
+//     if(i%2 == 0){
+//         console.log(`${i} is even`);
+//     }else{
+//         console.log(`${i} is odd`);
+//     }
+// }
+
+// task 05
+// let subject1 = +prompt("enter 1st subject marks");
+// let subject2 = +prompt("enter 2nd subject marks");
+// let subject3 = +prompt("enter 3rd subject marks");
+// let subject4 = +prompt("enter 4th subject marks");
+// let subject5 = +prompt("enter 5th subject marks");
+
+// let totalMarks = 500;
+// let obtMarks = subject1+ subject2+subject3+subject4+subject5;
+// let average = obtMarks/5
+
+// let grade;
+// if(average>=90 && average<=100){
+//     grade = "A"
+// }else if(average>=80 && average<90){
+//     grade = "B"
+// }else if(average>=70 && average<80){
+//     grade = "C"
+// }else if(average>=60 && average<70){
+//     grade = "D"
+// }else{
+//     grade = "F"
+// }
+// console.log("you secured ", grade, " grade");
+
+// TASK 06
+// for(let i=1; i<=100; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log("fizzBuzz")
+//     }else if(i%3==0){
+//         console.log("fizz")
+//     }
+//     else if(i%5==0){
+//         console.log("buzz")
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// TASK 07
+// for(let i=0; i<5; i++){
+//     let stars = "";
+//     for(let j=0; j<=i;j++){
+//         stars+="*"
+//     }
+//     console.log(stars)
+// }
